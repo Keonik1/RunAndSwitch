f3a76d9f1965562dea42db10459b7abe9d10716f
Keonik <balovnev01@gmail.com>
2022-06-12 22:22:03 +0500

    Change movement: Deleted second type of movement; In first type added more controlled movement Added rotation.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,51 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _defaultSpeed = 40;
-    [SerializeField] private MovementMethod _movementMethod;
 
     private float _horizontalInput;
     private float _verticalInput;
 
-    private Rigidbody _rigidbody;
-
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
-
     private void Update()
     {
-        float speed = _defaultSpeed;
         _verticalInput = Input.GetAxis("Vertical");
         _horizontalInput = Input.GetAxis("Horizontal");
 
-        Move(_movementMethod, speed);
-    }
-    private void Move(MovementMethod movementMethod, float speed)
-    {
-        switch (movementMethod)
-        {
-            case MovementMethod.ThroughANewVector:
-                Vector3 movementVector = new Vector3(_horizontalInput, 0, _verticalInput).normalized;
-                transform.Translate(movementVector * speed * Time.deltaTime);
-                break;
-            case MovementMethod.ThroughSpeedChange:
-                if (_verticalInput != 0 && _horizontalInput != 0)
-                    speed = _defaultSpeed / Mathf.Sqrt(2);
-    
-                transform.Translate(Vector3.forward * speed * Time.deltaTime * _verticalInput);
-                transform.Translate(Vector3.right * speed * Time.deltaTime * _horizontalInput);
-                break;
-        }
+        Move();
     }
 
-    private enum MovementMethod
+    private void Move()
     {
-        ThroughANewVector, ThroughSpeedChange
+        Vector3 movementVector = new Vector3(_horizontalInput, 0, _verticalInput).normalized;
+        if (movementVector == Vector3.zero)
+            return;
+
+        SetDirection(movementVector);
+        float axisInput = Mathf.Abs(_horizontalInput) + Mathf.Abs(_verticalInput);
+        transform.Translate(Vector3.forward * _defaultSpeed * Time.deltaTime * Mathf.Clamp01(axisInput));
+    }
+    
+    private void SetDirection(Vector3 movementVector)
+    {
+        Vector3 direction = Vector3.RotateTowards(transform.position, movementVector, 45, 0);
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 }
